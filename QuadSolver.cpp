9a3db5296c14c780b4a99c91aeee8a4471f9f77b
tajordan <tajordan95@yahoo.com>
2017-04-11 20:39:30 -0400

    test A

--- QuadSolver.cpp
+++ QuadSolver.cpp
@@ -3,6 +3,8 @@
 
 #include "QuadSolver.h"
 
+//A
+
 // return smallest positive real root if it exists; if it does not, return very big number
 double quad_solve( double a, double b, double c ) {
   double d = b*b - 4.0 * a * c;
