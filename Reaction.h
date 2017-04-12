89a12b780a3801d39349b63ac99be588958df70d
tajordan <tajordan95@yahoo.com>
2017-04-12 17:25:43 -0400

    xs functions added

--- Reaction.h
+++ Reaction.h
@@ -32,7 +32,8 @@ class capture_reaction : public reaction {
      capture_reaction( double x ) : reaction(x) { rxn_name = "capture"; };
     ~capture_reaction() {};
 
-    virtual void sample( particle* p, std::stack<particle>* bank );
+	virtual double xs(double E) = 0;
+    virtual void sample( particle* p, std::stack<particle>* bank ) = 0;
 };
 
 class SE_capture_reaction : public capture_reaction {
@@ -42,20 +43,21 @@ class SE_capture_reaction : public capture_reaction {
     SE_capture_reaction( double x ) : capture_reaction( x );
    ~SE_capture_reaction() {}; 
 
+	double xs(double E) { return rxn_xs; };
     void sample(particle* p, std::stack<particle>* bank );
 };
 
 class CE_capture_reaction : public capture_reaction {
   private:
-
+	std::shared_ptr <caffeine> energy_dependence;
   public:
-    CE_capture_reaction( double x, std::shared_ptr< Caffeine > eng_dep )
-    : capture_reaction( x ), energy_dependence(eng-dep) ; 
+    CE_capture_reaction( double x, std::shared_ptr< caffeine > eng_dep )
+    : capture_reaction( x ), energy_dependence(eng_dep) ; 
    ~CE_capture_reaction() {};
 
-   void sample( particle* p, std::stack<particle>* bank );
+	double xs(double E) { return rxn_xs * energy_dependence->sample(E); };
+    void sample( particle* p, std::stack<particle>* bank );
 };
-
 
 class scatter_reaction : public reaction {
   private:
@@ -65,7 +67,8 @@ class scatter_reaction : public reaction {
        reaction(x), scatter_dist(D) { rxn_name = "scatter"; };
     ~scatter_reaction() {};
 
-     virtual void sample( particle* p, std::stack<particle>* bank );
+	 virtual double xs(double E) = 0;
+     virtual void sample( particle* p, std::stack<particle>* bank ) = 0;
 };
 
 class SE_scatter_reaction : scatter_reaction {
@@ -76,17 +79,19 @@ class SE_scatter_reaction : scatter_reaction {
     :  scatter_reaction( double x, std::shared_ptr< distribution<double> > D );
    ~SE_scatter_reaction() {};
 
+	double xs(double E) { return rxn_xs; };
     void sample( particle*p, std::stack<particle>* p );
 };
 
 class CE_scatter_reaction : scatter_reaction {
   private:
-
+	std::shared_ptr <caffeine> energy_dependence;
   public:
-    CE_scatter_reaction( double x, std::shared_ptr< distribution<double> > D, std::shared_ptr< Caffeine > eng_dep ) 
+    CE_scatter_reaction( double x, std::shared_ptr< distribution<double> > D, std::shared_ptr< caffeine > eng_dep ) 
     :  scatter_reaction( double x, std::shared_ptr< distribution<double> > D ), energy_dependence(eng_dep);
    ~CE_scatter_reaction() {};
 
+	double xs(double E) { return rxn_xs * energy_dependence->sample(E); };
     void sample( particle*p, std::stack<particle>* p );
 };
 
@@ -104,7 +109,8 @@ class fission_reaction : public reaction {
        };
     ~fission_reaction() {};
 
-    void sample( particle* p, std::stack<particle>* bank );
+	virtual double xs(double E) = 0;
+    virtual void sample( particle* p, std::stack<particle>* bank ) = 0;
 };
 
 class SE_fission_reaction : public reaction {
@@ -115,17 +121,19 @@ class SE_fission_reaction : public reaction {
        fission_reaction( double x, std::shared_ptr< distribution<int> > D ); 
     ~SE_fission_reaction() {};
 
+	double xs(double E) { return rxn_xs; };
     void sample( particle* p, std::stack<particle>* bank );
 };
 
 class CE_fission_reaction : public reaction {
   private:
-    
+    std::shared_ptr <caffeine> energy_dependence;
   public:
-     CE_fission_reaction( double x, std::shared_ptr< distribution<int> > D, std::shared_ptr< Caffeine > eng_dep ) :
+     CE_fission_reaction( double x, std::shared_ptr< distribution<int> > D, std::shared_ptr< caffeine > eng_dep ) :
        fission_reaction( double x, std::shared_ptr< distribution<int> > D ), energy_dependence(eng_dep); 
     ~CE_fission_reaction() {};
 
+	double xs(double E) { return rxn_xs * energy_dependence->sample(E); };
     void sample( particle* p, std::stack<particle>* bank );
 };
 
