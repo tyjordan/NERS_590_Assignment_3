ee97ef738e7648911853000e922d4903ce472d49
tajordan <tajordan95@yahoo.com>
2017-04-13 23:00:04 -0400

    fixed cone with fixed reactions

--- Surface.cpp
+++ Surface.cpp
@@ -161,7 +161,7 @@ double x_cone::distance( ray r) {
 	double dz = r.pos.z - pos.z;
 	
 	double a = -(rad * rad)*(r.dir.x * r.dir.x) + (r.dir.y * r.dir.y) + (r.dir.z * r.dir.z);
-	double b = 2 * (-(rad * rad)*(dx * dx) + (dy * dy) + (dz * dz));
+	double b = 2 * ( (rad*rad)*(r.pos.x*r.dir.x - pos.x*r.dir.x) - (r.pos.y*r.dir.y) - (r.pos.z*r.dir.z) );
 	double c = eval(r.pos);
 	
 	return quad_solve(a, b, c);
